38721534e5760597c3904187a21f7d8063b77278
Georgi Todorov <georgiit98@gmail.com>
2023-06-26 20:56:32 +0300

    Home page navigation pages
    I make automation test for the button elements on the home page. I Assert that the links are correct and the some random label or title.

--- DemoQA/UnitTest1.cs
+++ DemoQA/UnitTest1.cs
@@ -10,7 +10,9 @@ namespace DemoQA
         public void Setup()
         {
             ChromeOptions options = new ChromeOptions();
-            options.BinaryLocation = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+            options.BinaryLocation = "/Users/georgi/Desktop/Google Chrome.app/Contents/MacOS/Google Chrome";
+            // for mac "/Users/georgi/Desktop/Google Chrome.app/Contents/MacOS/Google Chrome"
+            // for win "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
             driver = new ChromeDriver(options);
             driver.Manage().Window.Maximize();
         }
@@ -22,70 +24,80 @@ namespace DemoQA
             Thread.Sleep(2000);
 
             //Elements page
-            driver.FindElement(By.CssSelector("#app > div > div > div.home-body > div > div:nth-child(1) > div > div.avatar.mx-auto.white")).Click();
+            driver.FindElement(By.CssSelector("#app > div > div > div.home-body > div > div:nth-child(1)")).Click();
             Thread.Sleep(1000);
-            string randomTextFromThePage = driver.FindElement(By.XPath("//*[@id=\"app\"]/div/div/div[2]/div[2]/text()")).Text;
+            string randomTextFromThePage = driver.FindElement(By.CssSelector("#app > div > div > div.row > div.col-12.mt-4.col-md-6")).Text;
 
-            driver.Navigate().Back();
-            Thread.Sleep(1000);
 
             Assert.AreEqual(driver.Url, "https://demoqa.com/elements");
             Assert.AreEqual(randomTextFromThePage, "Please select an item from left to start practice.");
 
-            //Forms page
-            driver.FindElement(By.CssSelector("#app > div > div > div.home-body > div > div:nth-child(2) > div > div.avatar.mx-auto.white")).Click();
-            Thread.Sleep(1000);
-            randomTextFromThePage = driver.FindElement(By.XPath("//*[@id=\"app\"]/div/div/div[2]/div[2]/text()")).Text;
-
             driver.Navigate().Back();
             Thread.Sleep(1000);
+
+            
+            
+
+            //Forms page
+            driver.FindElement(By.CssSelector("#app > div > div > div.home-body > div > div:nth-child(2)")).Click();
+            Thread.Sleep(1000);
+            randomTextFromThePage = driver.FindElement(By.CssSelector("#app > div > div > div.row > div.col-12.mt-4.col-md-6")).Text;
 
             Assert.AreEqual(driver.Url, "https://demoqa.com/forms");
             Assert.AreEqual(randomTextFromThePage, "Please select an item from left to start practice.");
 
-            //AlertsWindows page
-            driver.FindElement(By.CssSelector("#app > div > div > div.home-body > div > div:nth-child(3) > div > div.card-up")).Click();
-            Thread.Sleep(1000);
-            randomTextFromThePage = driver.FindElement(By.XPath("//*[@id=\"app\"]/div/div/div[2]/div[2]/text()")).Text;
-
             driver.Navigate().Back();
             Thread.Sleep(1000);
+
+            
+
+            //AlertsWindows page
+            driver.FindElement(By.CssSelector("#app > div > div > div.home-body > div > div:nth-child(3)")).Click();
+            Thread.Sleep(1000);
+            randomTextFromThePage = driver.FindElement(By.CssSelector("#app > div > div > div.row > div.col-12.mt-4.col-md-6")).Text;
 
             Assert.AreEqual(driver.Url, "https://demoqa.com/alertsWindows");
             Assert.AreEqual(randomTextFromThePage, "Please select an item from left to start practice.");
 
-
-            //Widgets page
-            driver.FindElement(By.CssSelector("#app > div > div > div.home-body > div > div:nth-child(4) > div > div.card-body")).Click();
-            Thread.Sleep(1000);
-            randomTextFromThePage = driver.FindElement(By.XPath("//*[@id=\"app\"]/div/div/div[2]/div[2]/text()")).Text;
-
             driver.Navigate().Back();
             Thread.Sleep(1000);
+
+
+            //Widgets page
+            driver.FindElement(By.CssSelector("#app > div > div > div.home-body > div > div:nth-child(4)")).Click();
+            Thread.Sleep(1000);
+            randomTextFromThePage = driver.FindElement(By.CssSelector("#app > div > div > div.row > div.col-12.mt-4.col-md-6")).Text;
 
             Assert.AreEqual(driver.Url, "https://demoqa.com/widgets");
             Assert.AreEqual(randomTextFromThePage, "Please select an item from left to start practice.");
 
-            //Interaction page
-            driver.FindElement(By.CssSelector("#app > div > div > div.home-body > div > div:nth-child(5) > div > div.avatar.mx-auto.white > svg")).Click();
-            Thread.Sleep(1000);
-            randomTextFromThePage = driver.FindElement(By.XPath("//*[@id=\"app\"]/div/div/div[2]/div[2]/text()")).Text;
-
             driver.Navigate().Back();
             Thread.Sleep(1000);
+
+            
+
+            //Interaction page
+            driver.FindElement(By.CssSelector("#app > div > div > div.home-body > div > div:nth-child(5)")).Click();
+            Thread.Sleep(1000);
+            randomTextFromThePage = driver.FindElement(By.CssSelector("#app > div > div > div.row > div.col-12.mt-4.col-md-6")).Text;
 
             Assert.AreEqual(driver.Url, "https://demoqa.com/interaction");
             Assert.AreEqual(randomTextFromThePage, "Please select an item from left to start practice.");
 
-            //books page
-            driver.FindElement(By.CssSelector("#app > div > div > div.home-body > div > div:nth-child(6) > div > div.card-up")).Click();
-            Thread.Sleep(1000);
-            randomTextFromThePage = driver.FindElement(By.XPath("//*[@id=\"app\"]/div/div/div[1]/div")).Text;
             driver.Navigate().Back();
             Thread.Sleep(1000);
 
+
+            //books page
+            driver.FindElement(By.CssSelector("#app > div > div > div.home-body > div > div:nth-child(6)")).Click();
+            Thread.Sleep(1000);
+            randomTextFromThePage = driver.FindElement(By.CssSelector("#app > div > div > div.pattern-backgound.playgound-header > div")).Text;
+
             Assert.AreEqual(driver.Url, "https://demoqa.com/books");
             Assert.AreEqual(randomTextFromThePage, "Book Store");
+
+            driver.Navigate().Back();
+            Thread.Sleep(1000);
 
 
 
